008267a283c9838d85de4ad3647795b7412c672e
jerry <cle.ee@163.com>
2017-05-20 21:41:43 +0200

    add nodejs related module

---
+++ local.js
@@ -0,0 +1,9 @@
+var path = require('path'), express = require('express');
+var app = express();
+app.use('/ui5', express.static(path.join(__dirname, 'webapp')));
+app.get('/', function(req, res){
+   res.send("Hello World");
+});
+app.listen(process.env.PORT || 3000, function(){
+     console.log("Example app listens on port 3000.");
+});
