bcdcac2ab8d506acfadc3c2565074dc3939f3664
i042416 <cle.ee@163.com>
2017-07-15 08:40:23 +0800

    how to parse form data?

--- local.js
+++ local.js
@@ -1,4 +1,5 @@
 var path = require('path'), express = require('express');
+var qs = require('querystring');
 var app = express();
 app.use('/ui5', express.static(path.join(__dirname, 'webapp')));
 app.get('/', function(req, res){
@@ -7,10 +8,24 @@ app.get('/', function(req, res){
 });
 
 app.post("/", function(req, res){
-	console.log("method: " + req.method);
+	var body = '';
+	/*console.log("method: " + req.method);
     res.send("Hello World post");
     req.on('data', function(data) {
         console.log("request data: " + data);
+    });*/
+	req.on('data', function (data) {
+            body += data;
+
+            // Too much POST data, kill the connection!
+            // 1e6 === 1 * Math.pow(10, 6) === 1 * 1000000 ~~~ 1MB
+            if (body.length > 1e6)
+                request.connection.destroy();
+        });
+
+    req.on('end', function () {
+            var post = qs.parse(body);
+            res.send("Hello World result");
     });
 });
 app.listen(process.env.PORT || 3000, function(){
