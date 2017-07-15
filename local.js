4673a1ac7cf7ace692dd0dc9a9e1af25fbddb814
i042416 <cle.ee@163.com>
2017-07-15 08:24:09 +0800

    works for http post

--- local.js
+++ local.js
@@ -2,7 +2,13 @@ var path = require('path'), express = require('express');
 var app = express();
 app.use('/ui5', express.static(path.join(__dirname, 'webapp')));
 app.get('/', function(req, res){
+	console.log("method: " + req.method);
    res.send("Hello World");
+});
+
+app.post("/", function(req, res){
+	console.log("method: " + req.method);
+   res.send("Hello World post");
 });
 app.listen(process.env.PORT || 3000, function(){
      console.log("Example app listens on port 3000.");
